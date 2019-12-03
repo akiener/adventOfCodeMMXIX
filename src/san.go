6a4f733d43b045284b5ef33a0f30cf9aa6878f41
Albert Kiener <albertkiener94@gmail.com>
2019-12-04 00:54:31 +0100

    day 3 part 2 tests run, but result is flawed

--- src/san.go
+++ src/san.go
@@ -11,7 +11,7 @@ type Pos struct {
 	y int
 }
 
-func San(input string) int {
+func San(input string) (int, int) {
 	lines := strings.Split(input, "\n")
 	movements := make([][]string, 2)
 	for i := 0; i < 2; i++ {
@@ -92,7 +92,82 @@ func San(input string) int {
 
 	fmt.Println("part1:", minManhattenDistance)
 
-	return minManhattenDistance
+	stepsUntilIntersection1 := 0
+	prev0 = Pos{}
+	for _, move0 := range paths[0] {
+		prev1 := Pos{}
+		intersectionFound := false
+		stepsUntilIntersection1 = 0
+		for _, move1 := range paths[1] {
+			if prev0.x != move0.x &&
+				prev1.y != move1.y &&
+				((prev0.x < move1.x && move1.x < move0.x) || (prev0.x > move1.x && move1.x > move0.x)) &&
+				((prev1.y < move0.y && move0.y < move1.y) || (prev1.y > move0.y && move0.y > move1.y)) {
+				stepsUntilIntersection1 += abs(abs(move0.y) - abs(prev1.y))
+				intersectionFound = true
+				break
+			} else if prev0.y != move0.y &&
+				prev1.x != move1.x &&
+				((prev0.y < move1.y && move1.y < move0.y) || (prev0.y > move1.y && move1.y > move0.y)) &&
+				((prev1.x < move0.x && move0.x < move1.x) || (prev1.x > move0.x && move0.x > move1.x)) {
+				intersections = append(intersections, Pos{
+					x: move1.y,
+					y: move0.x,
+				})
+				stepsUntilIntersection1 += abs(abs(move0.x) - abs(prev1.x))
+				intersectionFound = true
+				break
+			} else {
+				stepsUntilIntersection1 += abs(abs(move1.x) - abs(prev1.x) + abs(move1.y) - abs(prev1.y))
+			}
+			prev1 = move1
+		}
+		if intersectionFound {
+			break
+		}
+		prev0 = move0
+	}
+
+	stepsUntilIntersection0 := 0
+	prev1 := Pos{}
+	for _, move1 := range paths[1] {
+		prev0 := Pos{}
+		intersectionFound := false
+		stepsUntilIntersection0 = 0
+		for _, move0 := range paths[0] {
+			if prev0.x != move0.x &&
+				prev1.y != move1.y &&
+				((prev0.x < move1.x && move1.x < move0.x) || (prev0.x > move1.x && move1.x > move0.x)) &&
+				((prev1.y < move0.y && move0.y < move1.y) || (prev1.y > move0.y && move0.y > move1.y)) {
+				stepsUntilIntersection0 += abs(abs(move1.x) - abs(prev0.x))
+				intersectionFound = true
+				break
+			} else if prev0.y != move0.y &&
+				prev1.x != move1.x &&
+				((prev0.y < move1.y && move1.y < move0.y) || (prev0.y > move1.y && move1.y > move0.y)) &&
+				((prev1.x < move0.x && move0.x < move1.x) || (prev1.x > move0.x && move0.x > move1.x)) {
+				intersections = append(intersections, Pos{
+					x: move1.y,
+					y: move0.x,
+				})
+				stepsUntilIntersection0 += abs(abs(move1.y) - abs(prev0.y))
+				intersectionFound = true
+				break
+			} else {
+				stepsUntilIntersection0 += abs(abs(move0.x) - abs(prev0.x) + abs(move0.y) - abs(prev0.y))
+			}
+			prev0 = move0
+		}
+		if intersectionFound {
+			break
+		}
+		prev1 = move1
+	}
+
+	combinedSteps := stepsUntilIntersection0 + stepsUntilIntersection1
+
+	fmt.Println("part2:", combinedSteps)
+	return minManhattenDistance, combinedSteps
 }
 
 func abs(x int) int {
--- src/san_test.go
+++ src/san_test.go
@@ -5,29 +5,53 @@ import "testing"
 func Test0(t *testing.T) {
 	input := `R8,U5,L5,D3
 U7,R6,D4,L4`
-	result := San(input)
-	const expected = 6
-	if result != expected {
-		t.Errorf("expected: %d, got: %d", expected, result)
+	manhattenDistance, combinedSteps := San(input)
+	{
+		const expected = 6
+		if manhattenDistance != expected {
+			t.Errorf("expected: %d, got: %d", expected, manhattenDistance)
+		}
+	}
+	{
+		const expected = 30
+		if combinedSteps != expected {
+			t.Errorf("expected: %d, got: %d", expected, combinedSteps)
+		}
 	}
 }
 
 func Test1(t *testing.T) {
 	input := `R75,D30,R83,U83,L12,D49,R71,U7,L72
 U62,R66,U55,R34,D71,R55,D58,R83`
-	result := San(input)
-	const expected = 159
-	if result != expected {
-		t.Errorf("expected: %d, got: %d", expected, result)
+	manhattenDistance, combinedSteps := San(input)
+	{
+		const expected = 159
+		if manhattenDistance != expected {
+			t.Errorf("expected: %d, got: %d", expected, manhattenDistance)
+		}
+	}
+	{
+		const expected = 610
+		if combinedSteps != expected {
+			t.Errorf("expected: %d, got: %d", expected, combinedSteps)
+		}
 	}
 }
 
 func Test2(t *testing.T) {
 	input := `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
 U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`
-	result := San(input)
-	const expected = 135
-	if result != expected {
-		t.Errorf("expected: %d, got: %d", expected, result)
+	manhattenDistance, combinedSteps := San(input)
+	{
+		const expected = 135
+		if manhattenDistance != expected {
+			t.Errorf("expected: %d, got: %d", expected, manhattenDistance)
+		}
+	}
+	{
+		const expected = 410
+		if combinedSteps != expected {
+			t.Errorf("expected: %d, got: %d", expected, combinedSteps)
+		}
 	}
 }
