bb6f35b996e3ae6ed74cbf6754a83d74550f9e59
Albert Kiener <albertkiener94@gmail.com>
2019-12-04 22:51:39 +0100

    day 4 part 2

--- src/yon.go
+++ src/yon.go
@@ -6,29 +6,69 @@ func Yon() {
 	var min = 382345
 	var max = 843167
 
-	var countValid = 0
-	for i := min; i <= max; i++ {
-		arr := []int{
-			i / 100000,
-			i / 10000 % 10,
-			i / 1000 % 10,
-			i / 100 % 10,
-			i / 10 % 10,
-			i % 10,
-		}
+	{
+		var countValid = 0
+		for i := min; i <= max; i++ {
+			arr := []int{
+				i / 100000,
+				i / 10000 % 10,
+				i / 1000 % 10,
+				i / 100 % 10,
+				i / 10 % 10,
+				i % 10,
+			}
 
-		valid := adjacentMatch(arr) && digitsRise(arr) && is6DigitNumber(arr)
-		if valid {
-			countValid++
+			valid := adjacentMatch(arr) && digitsRise(arr) && is6DigitNumber(arr)
+			if valid {
+				countValid++
+			}
 		}
+		fmt.Println("part1:", countValid)
 	}
 
-	fmt.Println("part1:", countValid)
+	{
+		var countValid = 0
+		for i := min; i <= max; i++ {
+			arr := []int{
+				i / 100000,
+				i / 10000 % 10,
+				i / 1000 % 10,
+				i / 100 % 10,
+				i / 10 % 10,
+				i % 10,
+			}
+
+			valid := adjacentMatchExactly2Long(arr) && digitsRise(arr) && is6DigitNumber(arr)
+			if valid {
+				countValid++
+			}
+		}
+		fmt.Println("part2:", countValid)
+	}
 }
 
 func adjacentMatch(p []int) bool {
 	for i := 0; i < len(p)-1; i++ {
 		if p[i] == p[i+1] {
+			return true
+		}
+	}
+	return false
+}
+
+func adjacentMatchExactly2Long(p []int) bool {
+	var prevMatch = -1
+	for i := 0; i+1 < len(p); i++ {
+		if p[i] == prevMatch {
+			continue
+		}
+		if p[i] == p[i+1] {
+			if i+2 < len(p) {
+				if p[i] == p[i+2] {
+					prevMatch = p[i]
+					continue
+				}
+			}
 			return true
 		}
 	}
